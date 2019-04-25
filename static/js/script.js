94da55a892d237e82f0d9d95005abde4cc1597c1
olof <olof@takman.se>
2019-04-25 16:53:48 +0200

    arbetade med progressbar

--- static/js/script.js
+++ static/js/script.js
@@ -3,35 +3,47 @@ $(document).ready(function() {
     $('#questions_3').hide();
     var i = 1
     $('#next').click(function(){
-        if (i == 1) {    
+
+        if (i == 1) {
+            i = 2
             $('#questions_1').hide(300);
             $('#questions_2').show(300);
-            i = 2
+            var element = document.getElementById("progress_bar");
+            var width = 0;
+            var identity = setInterval(scene_1, 10);
+            function scene_1() {
+                if (width >= 33) {
+                    clearInterval(identity);
+                } else {
+                    width++;
+                    element.style.width = width + '%';
+                };
+            }
+
+
         } else if (i == 2) {
+            i = 3
             $('#questions_2').hide(300);
             $('#questions_3').show(300);
             $('#next').hide(300);
-            i = 3
+            var element = document.getElementById("progress_bar");
+            var width = 33;
+            var identity = setInterval(scene_2, 10);
+            function scene_2() {
+                if (width >= 66) {
+                    clearInterval(identity);
+                } else {
+                    width++;
+                    element.style.width = width + '%';
+                };
+            }
+
+
         } else {
             $('#questions_3').hide(300);
         }
     });
 });
-
-function update() { 
-    var element = document.getElementById("myprogressBar");    
-    var width = 1; 
-    var identity = setInterval(scene, 10); 
-    function scene() { 
-        if (width >= 100) { 
-            clearInterval(identity); 
-        } else { 
-            width++;  
-            element.style.width = width + '%';  
-            element.innerHTML = width * 1  + '%'; 
-        } 
-    } 
-} 
 
 /*
 $(document).ready(function(){
