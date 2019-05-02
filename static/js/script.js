e4685a79439fb2a7ab51828b3945820d6fa9d06a
Beaudouin Nsolo <40185704+Swartan@users.noreply.github.com>
2019-05-02 15:36:07 +0200

    Addat smooth scrolling for give me a recipe

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,30 @@
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+    
+        
+        if (this.hash !== "") {
+        
+        event.preventDefault();
+    
+        // Store hash
+        var hash = this.hash;
+    
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+    
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+        } // End if
+    });
+    });
+
+
+
 $(document).ready(function() {
     $('#questions_2').hide();
     $('#questions_3').hide();
@@ -62,30 +89,3 @@ $(document).ready(function() {
 });
 
     
-
-/*
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-    
-        
-        if (this.hash !== "") {
-        
-        event.preventDefault();
-    
-        // Store hash
-        var hash = this.hash;
-    
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function(){
-    
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-        });
-        } // End if
-    });
-    });
-*/
