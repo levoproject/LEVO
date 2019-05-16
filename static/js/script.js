f9225c77922abbd5986675680e834c080b589ec7
olof <olof@takman.se>
2019-05-16 10:38:58 +0200

    Tog bort död kod

--- static/js/script.js
+++ static/js/script.js
@@ -157,40 +157,4 @@ $(document).ready(function() {
         });
     });
 
-    /*$("input(type='checkbox')").change(function() {
-        if($(this).is(":checked")){
-            $(this).parent().removeClass(".checkbox_item");
-            $(this).parent().addClass("green_background"); 
-        }else{
-            $(this).parent().removeClass("green_background");  
-        }
-    });*/
-
 });
-
-/*
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-    
-        
-        if (this.hash !== "") {
-        
-        event.preventDefault();
-    
-        // Store hash
-        var hash = this.hash;
-    
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function(){
-    
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-        });
-        } // End if
-    });
-    });
-*/
